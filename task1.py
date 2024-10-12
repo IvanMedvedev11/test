87310eae701309a9b011bfcdffc3c9de3922b93a
IvanMedvedev11 <medvivan11@gmail.com>
2024-10-12 18:00:06 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,4 +1,8 @@
-numbers_ = list(map(int, input("Введите список чисел: ").split()))
+try:
+    numbers_ = list(map(int, input("Введите список чисел: ").split()))
+except ValueError:
+    print("Ты И Д И О Т")
+    quit()
 result = 0
 for elem in numbers_:
     if elem >= 0:
